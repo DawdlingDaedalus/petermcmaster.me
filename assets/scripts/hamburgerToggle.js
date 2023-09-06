c2a8760ffe08c1a6c29b558dc96d720a77af5e2b
Peter McMaster <39925732+DawdlingDaedalus@users.noreply.github.com>
2023-09-05 18:33:11 -0700

    Complete navbar desktop/mobile 2

--- assets/scripts/hamburgerToggle.js
+++ assets/scripts/hamburgerToggle.js
@@ -1,6 +1,18 @@
 const mobileMenuButton = document.getElementById("mobile-menu");
 const navList = document.querySelector(".nav-list");
+const navLinks = document.querySelectorAll(".nav-list a");
+const navBrand = document.querySelector(".nav-brand");
 
 mobileMenuButton.addEventListener("click", function() {
     navList.classList.toggle("active");
+});
+
+navLinks.forEach(function(link) {
+    link.addEventListener("click", function() {
+        navList.classList.remove("active");
+    });
+});
+
+navBrand.addEventListener("click", function() {
+    navList.classList.remove("active");
 });
