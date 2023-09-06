214986d050913676c61dae15e4adfc6155365821
Peter McMaster <39925732+DawdlingDaedalus@users.noreply.github.com>
2023-09-06 16:40:15 -0700

    Complete Greeting with Background image

--- assets/scripts/navbarToggle.js
+++ assets/scripts/navbarToggle.js
@@ -20,12 +20,17 @@ function throttle(func, wait) {
 }
 
 let lastScrollTop = 0;
-const tolerance = 100;  // Adjust this value based on your needs. This means navbar will react after 50 pixels of scroll
+const tolerance = 100;  // Adjust this value based on your needs. navbar will react after 50 pixels of scroll
 
 const handleNavbar = throttle(function() {
     // Detects if on mobile view
     if(window.innerWidth <= 768) {
         let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        // Always show if at top of page
+        if (currentScrollTop < 300) {
+            document.querySelector(".navbar").classList.remove("hide");
+            return;
+        }
         // Check for tolerance
         if (Math.abs(lastScrollTop - currentScrollTop) <= tolerance) return;
         
@@ -38,6 +43,6 @@ const handleNavbar = throttle(function() {
         }
         lastScrollTop = currentScrollTop;
     }
-}, 200);  // Throttle for 200 milliseconds. You can adjust this value as needed
+}, 300);  // Throttle for 200 milliseconds. You can adjust this value as needed
 
 window.addEventListener("scroll", handleNavbar, false);
---
+++ assets/scripts/parallax.js
@@ -0,0 +1,22 @@
+let scrolled = 0;
+let lastScrollY = window.scrollY;
+
+function updateParallax() {
+    const parallaxDiv = document.querySelector(".parallax-background");
+    
+    let position = scrolled * 0.7;
+    
+    parallaxDiv.style.transform = `translateY(${position}px)`;
+    requestAnimationFrame(updateParallax);
+}
+
+document.addEventListener("scroll", function() {
+    scrolled = window.scrollY;
+
+    if (Math.abs(scrolled - lastScrollY) > 4) { // tweak this value for performance vs responsiveness
+        lastScrollY = scrolled;
+        requestAnimationFrame(updateParallax);
+    }
+});
+
+requestAnimationFrame(updateParallax);
